f3dd427ca70d1ddf10ba8311713e60ea5e2ca5f1
OdarkaDetroit <dasha.shmyelova@outlook.com>
2021-05-31 12:42:00 +0300

    First commit - it works with 2 tables on the design

---
+++ Library/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1059, 99);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Controls.Add(this.dataGridView2);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 97);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1059, 440);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(77, 228);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(466, 163);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(77, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(466, 163);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 537);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "MainMenu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Library/Form1.cs
@@ -0,0 +1,136 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bitmap;
+
+        MySqlConnection sqlCon = new MySqlConnection();
+        MySqlCommand sqlCom1 = new MySqlCommand();
+        MySqlCommand sqlCom2 = new MySqlCommand();
+
+        DataTable sqlDt1 = new DataTable();
+        DataTable sqlDt2 = new DataTable();
+
+        MySqlDataReader sqlRd1;
+        MySqlDataReader sqlRd2;
+
+        String sqlQuery1;
+        String sqlQuery2;
+        MySqlDataAdapter DtA = new MySqlDataAdapter();
+
+        DataSet DS = new DataSet();
+
+        String server = "localhost";
+        String username = "root";
+        String password = "u8s8jDddds2_0";
+        String database = "lib";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void upLoadData2()
+        {
+            sqlCon.ConnectionString = "server=" + server + ";"
+                                       + "user id=" + username + ";"
+                                       + "password=" + password + ";"
+                                       + "database=" + database;
+
+            sqlCon.Open();
+            sqlCom2.Connection = sqlCon;
+
+            sqlCom2.CommandText = @"select distinct ppk_author, initials
+                                    from author_book_connect inner join authors on second_name=ppk_author
+                                    where not exists(
+                                      select *
+                                        from reader
+                                        where (
+                                        (YEAR(CURRENT_DATE) - YEAR(birth_date)) -
+                                        (DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(birth_date, '%m%d')) 
+                                       >18 and not exists(
+                                        select *
+                                            from ((borrowing inner join exemplar on id_exemplar=ppk_exemplar)
+                                            inner join book on id_book =fk_book)
+                                            inner join author_book_connect as a on id_book=ppk_book
+                                        where a.ppk_author=author_book_connect.ppk_author
+                                             and reader.id_reader=ppk_reader
+                                            )));";
+
+            sqlRd2 = sqlCom2.ExecuteReader();
+
+            sqlDt2.Load(sqlRd2);
+
+            sqlRd2.Close();
+
+            sqlCon.Close();
+
+            dataGridView2.DataSource = sqlDt2;
+        }
+
+        private void upLoadData1()
+        {
+            sqlCon.ConnectionString = "server=" + server + ";"
+                           + "user id=" + username + ";"
+                           + "password=" + password + ";"
+                           + "database=" + database;
+
+            sqlCon.Open();
+            sqlCom1.Connection = sqlCon;
+
+            sqlCom1.CommandText = @"select ppk_author, count(id_book)
+                                    from author_book_connect inner join book
+                                    on id_book = ppk_book
+                                    where id_book in
+                                    
+                                    (select b.id_book
+                                     from book_catalogue_connet as b
+                                     where b.id_catalogue in 
+                                     
+                                     (select c.id_catalogue 
+                                      from system_catalogue as c
+                                      where catalogue_name = 'поезія'))
+
+                                      group by ppk_author; ";
+
+            sqlRd1 = sqlCom1.ExecuteReader();
+
+            sqlDt1.Load(sqlRd1);
+
+            sqlRd1.Close();
+
+            sqlCon.Close();
+
+            dataGridView1.DataSource = sqlDt1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            upLoadData1();
+            upLoadData2();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
