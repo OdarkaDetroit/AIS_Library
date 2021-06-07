b28888d3501ffa85bc1905303d234a0b207d9e68
Olha Zahoruiko <olya.zagoryiko13@gmail.com>
2021-06-07 20:44:16 +0300

    ComboBoxUpdate

--- Library/User/ViewBooks.Designer.cs
+++ Library/User/ViewBooks.Designer.cs
@@ -58,7 +58,6 @@ namespace Library
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(208, 33);
             this.comboBox1.TabIndex = 1;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // dataGridView1
             // 
