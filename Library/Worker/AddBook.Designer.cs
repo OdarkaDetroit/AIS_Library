918d1ab187fa253c50a84bab919d79cccc6559ae
OdarkaDetroit <dasha.shmyelova@outlook.com>
2021-06-17 13:13:55 +0300

    Worker changed design

--- Library/Worker/AddBook.Designer.cs
+++ Library/Worker/AddBook.Designer.cs
@@ -29,12 +29,233 @@ namespace Library.Worker
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.authorAdding = new System.Windows.Forms.Button();
+            this.authorEdit = new System.Windows.Forms.Button();
+            this.genreEdit = new System.Windows.Forms.Button();
+            this.genreAdding = new System.Windows.Forms.Button();
+            this.bookEdit = new System.Windows.Forms.Button();
+            this.bookAdding = new System.Windows.Forms.Button();
+            this.itemEdit = new System.Windows.Forms.Button();
+            this.itemAdding = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(641, 65);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(590, 190);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(641, 362);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 29;
+            this.dataGridView3.Size = new System.Drawing.Size(590, 190);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(25, 362);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.RowTemplate.Height = 29;
+            this.dataGridView4.Size = new System.Drawing.Size(590, 190);
+            this.dataGridView4.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(590, 190);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(25, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Перегляд авторів";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(641, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Перегляд жанрів";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(25, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Перегляд книг";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(641, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(251, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Перегляд примірників";
+            // 
+            // authorAdding
+            // 
+            this.authorAdding.Location = new System.Drawing.Point(25, 271);
+            this.authorAdding.Name = "authorAdding";
+            this.authorAdding.Size = new System.Drawing.Size(129, 29);
+            this.authorAdding.TabIndex = 9;
+            this.authorAdding.Text = "Додати автора";
+            this.authorAdding.UseVisualStyleBackColor = true;
+            // 
+            // authorEdit
+            // 
+            this.authorEdit.Location = new System.Drawing.Point(170, 271);
+            this.authorEdit.Name = "authorEdit";
+            this.authorEdit.Size = new System.Drawing.Size(129, 29);
+            this.authorEdit.TabIndex = 10;
+            this.authorEdit.Text = "Редагувати автора";
+            this.authorEdit.UseVisualStyleBackColor = true;
+            // 
+            // genreEdit
+            // 
+            this.genreEdit.Location = new System.Drawing.Point(786, 271);
+            this.genreEdit.Name = "genreEdit";
+            this.genreEdit.Size = new System.Drawing.Size(153, 29);
+            this.genreEdit.TabIndex = 12;
+            this.genreEdit.Text = "Редагувати автора";
+            this.genreEdit.UseVisualStyleBackColor = true;
+            // 
+            // genreAdding
+            // 
+            this.genreAdding.Location = new System.Drawing.Point(641, 271);
+            this.genreAdding.Name = "genreAdding";
+            this.genreAdding.Size = new System.Drawing.Size(129, 29);
+            this.genreAdding.TabIndex = 11;
+            this.genreAdding.Text = "Додати жанр";
+            this.genreAdding.UseVisualStyleBackColor = true;
+            // 
+            // bookEdit
+            // 
+            this.bookEdit.Location = new System.Drawing.Point(170, 567);
+            this.bookEdit.Name = "bookEdit";
+            this.bookEdit.Size = new System.Drawing.Size(155, 29);
+            this.bookEdit.TabIndex = 14;
+            this.bookEdit.Text = "Редагувати книгу";
+            this.bookEdit.UseVisualStyleBackColor = true;
+            // 
+            // bookAdding
+            // 
+            this.bookAdding.Location = new System.Drawing.Point(25, 567);
+            this.bookAdding.Name = "bookAdding";
+            this.bookAdding.Size = new System.Drawing.Size(129, 29);
+            this.bookAdding.TabIndex = 13;
+            this.bookAdding.Text = "Додати книгу";
+            this.bookAdding.UseVisualStyleBackColor = true;
+            // 
+            // itemEdit
+            // 
+            this.itemEdit.Location = new System.Drawing.Point(826, 567);
+            this.itemEdit.Name = "itemEdit";
+            this.itemEdit.Size = new System.Drawing.Size(188, 29);
+            this.itemEdit.TabIndex = 16;
+            this.itemEdit.Text = "Редагувати примірник";
+            this.itemEdit.UseVisualStyleBackColor = true;
+            // 
+            // itemAdding
+            // 
+            this.itemAdding.Location = new System.Drawing.Point(641, 567);
+            this.itemAdding.Name = "itemAdding";
+            this.itemAdding.Size = new System.Drawing.Size(169, 29);
+            this.itemAdding.TabIndex = 15;
+            this.itemAdding.Text = "Додати примірник";
+            this.itemAdding.UseVisualStyleBackColor = true;
+            // 
+            // AddBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1258, 641);
+            this.Controls.Add(this.itemEdit);
+            this.Controls.Add(this.itemAdding);
+            this.Controls.Add(this.bookEdit);
+            this.Controls.Add(this.bookAdding);
+            this.Controls.Add(this.genreEdit);
+            this.Controls.Add(this.genreAdding);
+            this.Controls.Add(this.authorEdit);
+            this.Controls.Add(this.authorAdding);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "AddBook";
             this.Text = "AddBook";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button authorAdding;
+        private System.Windows.Forms.Button authorEdit;
+        private System.Windows.Forms.Button genreEdit;
+        private System.Windows.Forms.Button genreAdding;
+        private System.Windows.Forms.Button bookEdit;
+        private System.Windows.Forms.Button bookAdding;
+        private System.Windows.Forms.Button itemEdit;
+        private System.Windows.Forms.Button itemAdding;
     }
 }
