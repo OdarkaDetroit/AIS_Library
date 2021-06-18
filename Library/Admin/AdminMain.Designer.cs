8dfa62ac5e239f299f5634e00c00758ae554322f
OdarkaDetroit <dasha.shmyelova@outlook.com>
2021-06-18 08:18:58 +0300

    colors

--- Library/Admin/AdminMain.Designer.cs
+++ Library/Admin/AdminMain.Designer.cs
@@ -32,7 +32,6 @@ namespace Library.Admin
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label7 = new System.Windows.Forms.Label();
             this.bookNameBox = new System.Windows.Forms.ComboBox();
@@ -73,20 +72,11 @@ namespace Library.Admin
             // 
             this.button1.Location = new System.Drawing.Point(183, 59);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 29);
+            this.button1.Size = new System.Drawing.Size(258, 29);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Оновити таблицю";
+            this.button1.Text = "Оновити таблицю боржників";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(362, 59);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 29);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Інфо про боржника";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
@@ -212,7 +202,6 @@ namespace Library.Admin
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(929, 602);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
@@ -233,7 +222,6 @@ namespace Library.Admin
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
--- Library/Worker/WorkerMain.Designer.cs
+++ Library/Worker/WorkerMain.Designer.cs
@@ -44,10 +44,9 @@ namespace Library.Worker
             // deletingBook
             // 
             this.deletingBook.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.deletingBook.Location = new System.Drawing.Point(30, 176);
-            this.deletingBook.Margin = new System.Windows.Forms.Padding(4);
+            this.deletingBook.Location = new System.Drawing.Point(0, 141);
             this.deletingBook.Name = "deletingBook";
-            this.deletingBook.Size = new System.Drawing.Size(186, 80);
+            this.deletingBook.Size = new System.Drawing.Size(173, 64);
             this.deletingBook.TabIndex = 0;
             this.deletingBook.Text = "Списання";
             this.deletingBook.UseVisualStyleBackColor = false;
@@ -56,10 +55,9 @@ namespace Library.Worker
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.button2.Location = new System.Drawing.Point(30, 62);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(0, 50);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(186, 80);
+            this.button2.Size = new System.Drawing.Size(173, 64);
             this.button2.TabIndex = 1;
             this.button2.Text = "Зареєструвати книгу";
             this.button2.UseVisualStyleBackColor = false;
@@ -68,10 +66,9 @@ namespace Library.Worker
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Info;
-            this.panel1.Location = new System.Drawing.Point(15, 15);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1230, 721);
+            this.panel1.Size = new System.Drawing.Size(447, 577);
             this.panel1.TabIndex = 2;
             // 
             // panel2
@@ -82,39 +79,41 @@ namespace Library.Worker
             this.panel2.Controls.Add(this.lendingBook);
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.deletingBook);
-            this.panel2.Location = new System.Drawing.Point(1241, 15);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Location = new System.Drawing.Point(233, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(244, 721);
+            this.panel2.Size = new System.Drawing.Size(195, 577);
             this.panel2.TabIndex = 0;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(30, 536);
+            this.button3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button3.Location = new System.Drawing.Point(0, 429);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(186, 80);
+            this.button3.Size = new System.Drawing.Size(173, 64);
             this.button3.TabIndex = 5;
             this.button3.Text = "Загублення";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 405);
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Location = new System.Drawing.Point(0, 324);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(186, 80);
+            this.button1.Size = new System.Drawing.Size(173, 64);
             this.button1.TabIndex = 4;
             this.button1.Text = "Повернення\r\nабо зміна терміну";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lendingBook
             // 
             this.lendingBook.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.lendingBook.Location = new System.Drawing.Point(30, 290);
-            this.lendingBook.Margin = new System.Windows.Forms.Padding(4);
+            this.lendingBook.Location = new System.Drawing.Point(0, 232);
             this.lendingBook.Name = "lendingBook";
-            this.lendingBook.Size = new System.Drawing.Size(186, 80);
+            this.lendingBook.Size = new System.Drawing.Size(173, 64);
             this.lendingBook.TabIndex = 2;
             this.lendingBook.Text = "Видача книг";
             this.lendingBook.UseVisualStyleBackColor = false;
@@ -128,12 +127,11 @@ namespace Library.Worker
             // 
             // WorkerMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1500, 751);
+            this.ClientSize = new System.Drawing.Size(476, 601);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "WorkerMain";
             this.Text = "WorkerMain";
             this.panel2.ResumeLayout(false);
